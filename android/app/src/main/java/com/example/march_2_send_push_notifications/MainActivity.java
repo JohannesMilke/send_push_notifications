0b8be27db5cbc9db998fd2710388a284c8a81404
Johannes Milke <johannesmilke1995@gmail.com>
2019-03-29 20:17:35 +0100

    init

---
+++ android/app/src/main/java/com/example/march_2_send_push_notifications/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.march_2_send_push_notifications;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
